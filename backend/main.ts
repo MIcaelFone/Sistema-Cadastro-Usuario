39612fdf4012b05d9351b44213ef2e6ab9837a50
Micael Fone <micaelfone0@gmail.com>
2025-06-07 17:56:08 -0300

    Arrumando e configurando o swagger

--- backend/main.ts
+++ backend/main.ts
@@ -1,19 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Enable CORS
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  
-  // Set global prefix
   app.setGlobalPrefix('api');
-  
+  const config = new DocumentBuilder()
+    .setTitle('Users API')
+    .setDescription('The users API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
--- backend/src/Controller/UserController.ts
+++ backend/src/Controller/UserController.ts
@@ -1,28 +1,40 @@
 import { Controller, Get, Post, Put, Delete, Body, Param, ParseIntPipe, HttpStatus } from '@nestjs/common';
 import { UserService } from './../Service/UserService';
 import { User } from '../model/UserModel';
-
+import { ApiTags,ApiResponse,ApiOperation } from '@nestjs/swagger';
+@ApiTags('users')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('/')
+  @ApiOperation({ summary: 'Buscar todos usuários' })
+  @ApiResponse({ status: 200, description: 'Usuário encontrado' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
   async findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Buscar usuário por ID' })
+  @ApiResponse({ status: 200, description: 'Usuário encontrado' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<User> {
     return this.userService.findOne(id);
   }
 
   @Post('/')
+  @ApiOperation({ summary: 'Criar um usuário' })
+  @ApiResponse({ status: 201, description: 'Usuário criado com sucesso' })
   async create(@Body() userData: Partial<User>): Promise<HttpStatus> {
     await this.userService.create(userData);
     return HttpStatus.CREATED;
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Usuário atualizado por ID' })
+  @ApiResponse({ status: 200, description: 'Usuário atualizado com sucesso' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
   async update(
     @Param('id', ParseIntPipe) id: number, 
     @Body() userData: Partial<User>
@@ -32,6 +44,9 @@ export class UserController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remover usuário por ID' })
+  @ApiResponse({ status: 200, description: 'Usuário removido com sucesso' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
   async remove(@Param('id', ParseIntPipe) id: number): Promise<HttpStatus> {
     this.userService.remove(id);
     return HttpStatus.OK;
